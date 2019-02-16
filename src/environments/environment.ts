6ea49f86ea3c6755a0a15e817bac30056fe71685
kvothemosser <moises.tellez.tapia@gmail.com>
2019-02-15 18:23:09 -0600

    Se agrega la configuración de firebase.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAc9Tt8pQQF3ntu2Tz66iUKl_fXHtdhIyc',
-    authDomain: 'firestarter-96e46.firebaseapp.com',
-    databaseURL: 'https://firestarter-96e46.firebaseio.com',
-    projectId: 'firestarter-96e46',
-    storageBucket: 'firestarter-96e46.appspot.com',
-    messagingSenderId: '581326886241'
+    apiKey: 'AIzaSyDqSAKALK0bcz-crJzxOTtm2XXEc11eCjw',
+    authDomain: 'kalaharitribe.firebaseapp.com',
+    databaseURL: 'https://kalaharitribe.firebaseio.com',
+    projectId: 'kalaharitribe',
+    storageBucket: 'kalaharitribe.appspot.com',
+    messagingSenderId: '450515700494'
   }
 };
 
